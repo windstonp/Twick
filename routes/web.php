5f2ce6ce22a2e67e68d840bb5881c0c60a0e40f8
alvez <joaor4e@gmail.com>
2021-01-09 15:02:59 -0300

    TK - created profile index page test

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,8 @@ Route::delete('/twicks/{id}/like',[App\Http\Controllers\TwickLikesController::cl
 
 Route::post('profile/{user:username}/follows',[App\Http\Controllers\FollowsController::class,'store']);
 Route::get('profile/{user:username}',[App\Http\Controllers\ProfileController::class,'show'])->name('profile');
-Route::get('profile/{user:username}/edit',[App\Http\Controllers\ProfileController::class,'edit']);
+Route::get('profile/{user:username}/edit',[App\Http\Controllers\ProfileController::class,'edit'])->name('profileEdit');
 Route::Put('profile/{user:username}/update',[App\Http\Controllers\ProfileController::class,'update']);
-Route::delete('profile/{user:username}/delete',[\App\Http\Controllers\ProfileController::class,'destroy']);
+Route::delete('profile/{user:username}/delete',[\App\Http\Controllers\ProfileController::class,'destroy'])->name('profileDestroy');
 
 Route::get('find',App\Http\Controllers\findUserController::class);
---
+++ tests/Feature/ProfileTests/ProfilePageTest.php
@@ -0,0 +1,12 @@
+<?php
+
+test('test profile page loads correctly', function () {
+    $user = userFactory()->create();
+
+    $response = actingAs($user)->get(route('profile',$user->username));
+
+    expect($user->twicks()->WithLikes()->paginate(5))->toBeInstanceOf('Illuminate\Pagination\LengthAwarePaginator');
+
+    $response->assertStatus(200);
+});
+
