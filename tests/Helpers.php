65882cac792a14135081e2225bfd922b938af36a
alvez <joaor4e@gmail.com>
2021-01-08 22:48:33 -0300

    edited actingAs helper method

--- tests/Helpers.php
+++ tests/Helpers.php
@@ -5,7 +5,7 @@ function get(string $url)
 {
     return test()->get($url);
 }
-function actingAs(Authenticable $user, $driver = NULL){
+function actingAs($user, $driver = NULL){
 
     test()->actingAs($user, $driver);
 
